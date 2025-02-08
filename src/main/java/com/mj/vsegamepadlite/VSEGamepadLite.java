016ca3b7c062bbccb11efca3ccba560dbc94cf05
MJPC83 <mujjamil8383@gmail.com>
2025-02-08 18:54:16 +0530

    Initial commit

--- src/main/java/com/mj/vsegamepadlite/VSEGamepadLite.java
+++ src/main/java/com/mj/vsegamepadlite/VSEGamepadLite.java
@@ -1,32 +1,40 @@
 package com.mj.vsegamepadlite;
 
-import net.minecraftforge.api.distmarker.Dist;
+import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
 
 @Mod(VSEGamepadLite.MOD_ID)
 public class VSEGamepadLite {
-
     public static final String MOD_ID = "vsegamepadlite";
 
-    private static GamepadHandler gamepadHandler;
+    private static final Logger LOGGER = LogUtils.getLogger();
+    private final GamepadHandler gamepadHandler;
 
     public VSEGamepadLite() {
-        if (FMLEnvironment.dist == Dist.CLIENT) {
+        // Initialize Gamepad Handler
+        gamepadHandler = new GamepadHandler();
 
-            gamepadHandler = new GamepadHandler();
+        // Register the common config
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ConfigHandler.COMMON_SPEC);
 
-            // Register the common config
-            ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ConfigHandler.COMMON_SPEC);
+        // Register lifecycle event listeners
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 
+        // Register ourselves to listen for other game events
+        MinecraftForge.EVENT_BUS.register(this);
+    }
 
-            MinecraftForge.EVENT_BUS.register(this);
-        }
+    private void setup(final FMLCommonSetupEvent event) {
+        LOGGER.info("VSEGamepad lite is setting up!");
     }
 
     @SubscribeEvent
